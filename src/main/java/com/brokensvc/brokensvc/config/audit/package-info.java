da495966af77d1ead76c134827c63ac61ae3b394
default <default>
2019-05-22 06:44:04 +0000

    Adding initial code structure

---
+++ src/main/java/com/brokensvc/brokensvc/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.brokensvc.brokensvc.config.audit;
---
+++ src/main/java/com/brokensvc/brokensvc/repository/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring Data JPA repositories.
+ */
+package com.brokensvc.brokensvc.repository;
